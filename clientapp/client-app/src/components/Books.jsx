7c7179aeb31d79bcd1c3c2b446c54f82ae48e6d7
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-18 14:46:14 +0000

    Render list of books

---
+++ clientapp/client-app/src/components/Books.jsx
@@ -0,0 +1,23 @@
+﻿import PropTypes from 'prop-types';
+
+const Books = ({ id, name, price, category, author }) => {
+    return (
+        <div className="book-card" id={id}>
+            <h2>{name}</h2>
+            <p><strong>ID:</strong> {id}</p>
+            <p><strong>Author:</strong> {author}</p>
+            <p><strong>Category:</strong> {category}</p>
+            <p><strong>Price:</strong> {price}</p>
+        </div>
+    )
+}
+
+Books.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired, 
+};
+
+export default Books;
--- clientapp/client-app/src/pages/BooksPage.jsx
+++ clientapp/client-app/src/pages/BooksPage.jsx
@@ -1,9 +1,46 @@
-﻿const BooksPage = () => {
+﻿import { useEffect, useState } from 'react';
+import Books from '../components/Books.jsx';
+
+const BooksPage = () => {
+    const [booksData, setBooksData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:5275/api/books');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setBooksData(data);
+            } catch (err) {
+                console.error('Error fetching books data:', err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(booksData);
+
     return (
         <>
             <h1 className="main_title">
                 List of books
             </h1>
+
+            {booksData.map((book) => {
+                return (
+                    <Books
+                        key={book.Id}
+                        id={book.Id}
+                        name={book.Name}
+                        price={book.Price}
+                        category={book.Category}
+                        author={book.Author}
+                    />
+                )
+            })}
         </>
     )
 }
