3ae7e757fcb863fdf17b3cf11e66b312386f97a5
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-20 23:52:51 +0000

    Initial commit for ISBN model

---
+++ Models/ISBN.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace RASP_Redis.Models
+{
+    public class ISBN
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Isbn { get; set; } = null!;
+        public string docId { get; set; } = null!;
+    }
+}
