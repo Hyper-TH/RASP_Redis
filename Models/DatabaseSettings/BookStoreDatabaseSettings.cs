9ae947852b6646f8eb67d5f856ca7f00707eff14
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-11-27 13:47:07 +0000

    Added database settings for ProjectA mongo db

---
+++ Models/DatabaseSettings/BookStoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace RASP_Redis.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BooksCollectionName { get; set; } = null!;
+        public string ISBNsCollectionName { get; set; } = null!;
+    }
+}
---
+++ Models/DatabaseSettings/ProjectADatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace RASP_Redis.Models.DatabaseSettings
+{
+    public class ProjectADatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string UsersCollectionName { get; set; } = null!;
+        public string MeetingsCollectionName { get; set; } = null!;
+        public string AttendeesCollectionName { get; set; } = null!;
+    }
+}
